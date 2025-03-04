62263557b56e9afd8d5502ffea84a57fc82feb96
AgnerLoss <agner1984@yahoo.com.br>
2025-03-03 21:58:59 -0300

    update security group

--- main.tf
+++ main.tf
@@ -2,39 +2,10 @@ provider "aws" {
   region = var.aws_region  # Usa a variável para definir a região
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "web-security-group"
-  description = "Allow HTTP, HTTPS, and Flask"
-
-  ingress {
-    description = "Allow Flask (porta 5000)"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso público
-  }
-
-  ingress {
-    description = "Allow SSH (para acesso via terminal)"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP para EC2 Instance Connect
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "app_server" {
   ami                    = "ami-094b981da55429bfc"  # AMI do Amazon Linux 2 (ou use um Ubuntu)
   instance_type          = "t2.micro"               # Tipo de instância Free Tier
-  security_groups        = [aws_security_group.web_sg.name]
+  security_groups        = ["sg-02473c56a241de8c5"]
   associate_public_ip_address = true  # Garante que a instância tenha IP público
 
   tags = {
